99e92a56bc6d6e767ac51d1e37ae496d13ca9be7
Priyanshu Kamal <priyanshukamal268@gmail.com>
2026-02-06 15:36:59 +0530

    Added a new file 5streamCode02Transform.js with code for transforming data using streams.

---
+++ 5streamCode02Transform.js
@@ -0,0 +1,13 @@
+const { Transform} = require('stream');
+const fs = require('fs');
+
+const upperCase = new Transform({
+    transform(chunk, encoding, callback) {
+        this.push(chunk.toString().toUpperCase());
+        callback();
+    }
+});
+
+fs.createReadStream('5streamCode01input.txt')
+    .pipe(upperCase)
+    .pipe(fs.createWriteStream('5streamCode02output.txt'));
